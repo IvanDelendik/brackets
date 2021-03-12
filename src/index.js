f6aad3cd85fef724dd90f3d199b3656777f3b74c
Delendik Ivan <vanya.delya@mail.ru>
2021-03-12 22:55:09 +0300

    feat: add result index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const map = new Map(bracketsConfig);
+    const array = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== map.get(array[array.length - 1])) {
+            array.push(str[i]);
+        } else array.pop();
+    }
+    return array.length === 0;
+};
